ae00d792a47f8741efbddcfcdaf93950c2be1657
Atakan Karaca <atakan.karaca@metu.edu.tr>
2025-08-13 02:34:48 +0300

    Refactor BasicAgent implementation to use workflow and GraphState for enhanced question handling

--- app.py
+++ app.py
@@ -3,23 +3,13 @@ import gradio as gr
 import requests
 import inspect
 import pandas as pd
+from src.workflow import workflow,GraphState
+
+from langchain_core.messages import HumanMessage
 
 # (Keep Constants as is)
 # --- Constants ---
 DEFAULT_API_URL = "https://agents-course-unit4-scoring.hf.space"
-
-
-# --- Basic Agent Definition ---
-# ----- THIS IS WERE YOU CAN BUILD WHAT YOU WANT ------
-class BasicAgent:
-    def __init__(self):
-        print("BasicAgent initialized.")
-
-    def __call__(self, question: str) -> str:
-        print(f"Agent received question (first 50 chars): {question[:50]}...")
-        fixed_answer = "This is a default answer."
-        print(f"Agent returning fixed answer: {fixed_answer}")
-        return fixed_answer
 
 
 def run_and_submit_all(profile: gr.OAuthProfile | None):
@@ -43,7 +33,7 @@ def run_and_submit_all(profile: gr.OAuthProfile | None):
 
     # 1. Instantiate Agent ( modify this part to create your agent)
     try:
-        agent = BasicAgent()
+        agent = workflow
     except Exception as e:
         print(f"Error instantiating agent: {e}")
         return f"Error initializing agent: {e}", None
@@ -83,7 +73,15 @@ def run_and_submit_all(profile: gr.OAuthProfile | None):
             print(f"Skipping item with missing task_id or question: {item}")
             continue
         try:
-            submitted_answer = agent(question_text)
+            initial_state = GraphState({
+                "messages": [HumanMessage(content=question_text)],
+                "tool_calls": [],
+                "question": question_text,
+                "task_id": task_id,
+                "file_name": item.get("file_name"),
+                "file_location": None,
+            })
+            submitted_answer = agent.invoke(initial_state)
             answers_payload.append(
                 {"task_id": task_id, "submitted_answer": submitted_answer}
             )
